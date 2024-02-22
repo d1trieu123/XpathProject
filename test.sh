92b53d42c656aff785e58c36aced52a1fd53f350
Allison-Turner <aturner@ucsd.edu>
2024-02-22 13:00:16 -0800

    reorganized project and wrote a basic Makefile and testing script. need to adapt the way that the code locates the input XML file, but made progress

---
+++ test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+CWD=$(pwd)
+BUILD_DIR="$CWD/build"
+
+#SRC="$CWD/src"
+#ANTLR_SRC="$SRC/antlr"
+#JAVA_SRC="$SRC/java"
+
+TEST="$CWD/test"
+TEST_DATA="$TEST/data"
+TEST_QUERIES="$TEST/queries"
+
+#JAVA_COMPILER=$(which javac)
+#JAVA_COMPILER_FLAGS= -g
+JVM_INVOCATION=$(which java)
+
+ANTLR4_JAR="/usr/local/lib/antlr-4.13.1-complete.jar"
+
+ENTRYPOINT=Main
+
+TEST_QUERIES=$(find $TEST_QUERIES -type f -name "*.txt")
+
+set -x;
+for f in $TEST_QUERIES;
+do
+
+    $JVM_INVOCATION -cp .:$ANTLR4_JAR:$BUILD_DIR $ENTRYPOINT $f
+    RET=$?
+
+    if [ $RET -ne 0 ]; then
+        echo "ERROR = $RET"
+        exit
+    fi
+
+done
+
+set +x;
